02395a95c5c75c05eb3a6f54c08ccfb1f1911850
Jerry9 <zbylutkonrad@gmail.com>
2021-05-02 22:51:36 +0200

    mySQL migration

---
+++ Cars/Configuration/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+using Cars.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+// to create new migration
+// create variable with new model e.g. "public virtual DbSet<Brand> Brands { get; set; }"
+// in console type "add-migration <name_of_migration>"
+// to update the database type "update-database" and it will use the latest migration
+namespace Cars.Configuration
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<CarRental> CarRentals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("Server=dot-cars.mysql.database.azure.com; Port=3306; Database=cars; User=system@dot-cars; Password=i7!3$uRmGtY&t#fjMgTr;",
+                new MySqlServerVersion(new Version(8, 0, 11)))
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                    .AddConsole()
+                    .AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
