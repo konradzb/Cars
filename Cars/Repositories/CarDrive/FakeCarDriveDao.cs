c59817799f2bbe30d136dd6be0e060539a807a84
Dominik Marszalek <marszalek.dominik@gmail.com>
2021-05-10 17:03:29 +0200

    Add CarDrive RestController, Model, Dto, Dao, Services

---
+++ Cars/Repositories/CarDrive/FakeCarDriveDao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cars.Model;
+
+namespace Cars.Repo
+{
+    public class FakeCarDriveDao : ICarDriveDao
+    {
+        private List<CarDrive> carDrives = new();
+        public FakeCarDriveDao()
+        {
+            carDrives.Add(new CarDrive(1, "FWD"));
+            carDrives.Add(new CarDrive(2, "RWD"));
+            carDrives.Add(new CarDrive(3, "AWD"));
+        }
+        public List<CarDrive> GetAllCarDrives()
+        {
+            return carDrives;
+        }
+
+        public CarDrive GetCarDriveById(int id)
+        {
+            return carDrives.Find(carDrive => carDrive.Id == id);
+        }
+    }
+}
---
+++ Cars/Service/CarDrive/CarDriveService.cs
@@ -0,0 +1,36 @@
+﻿using Cars.Dtos;
+using Cars.Model;
+using Cars.Repo;
+using Cars.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Service
+{
+    public class CarDriveService : ICarDriveService
+    {
+        private readonly ICarDriveDao carDrivesDao;
+        public CarDriveService(ICarDriveDao carDrivesDao)
+        {
+            this.carDrivesDao = carDrivesDao;
+        }
+        public IEnumerable<CarDriveDto> GetAllCarDrives()
+        {
+            var carDrives = carDrivesDao.GetAllCarDrives().ConvertAll(carDrive => carDrive.AsDto());
+            return carDrives;
+        }
+
+        public ActionResult<CarDriveDto> GetCarDriveById(int id)
+        {
+            var carDrive = carDrivesDao.GetCarDriveById(id);
+            if(carDrive is null)
+            {
+                return null;
+            }
+            return carDrive.AsDto();
+        }
+    }
+}
